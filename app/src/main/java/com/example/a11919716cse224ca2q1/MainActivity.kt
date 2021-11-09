b0a01f96418d54fc9430b22e3201e67a8a16ebd8
xuwudawei <xuwudawei@outlook.com>
2021-11-10 01:38:40 +0530

    updated UI

--- app/src/main/java/com/example/a11919716cse224ca2q1/MainActivity.kt
+++ app/src/main/java/com/example/a11919716cse224ca2q1/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.a11919716cse224ca2q1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val userInput=findViewById<EditText>(R.id.emailPhoneSkypeInput)
+        val  signInButton=findViewById<Button>(R.id.signInButton)
+        val joinMeetButton=findViewById<Button>(R.id.joinMeetingButton)
+        val signUpButton=findViewById<Button>(R.id.signUpButton)
+        signInButton.setOnClickListener{
+            val userDetail:String=userInput.text.toString()
+            Toast.makeText(applicationContext,"You entered: "+userDetail,Toast.LENGTH_SHORT).show()
+        }
     }
 }
